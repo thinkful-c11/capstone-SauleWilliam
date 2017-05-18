175ee18567e4510e6189374e2330a1a38236d043
Saule BN <beyonce@thinkful.com>
2017-05-18 12:33:50 -0400

    html added

---
+++ app.js
@@ -0,0 +1,20 @@
+const appState={
+    results: [],
+}
+
+const baseURL="https://tastedive.com/api/similar";
+/////////////Mod Functions////////////////////////
+function getDataFromApi(searchQuery,callback){
+    const query={
+      k:"270170-ThinfulC-SJOP8QJA",
+      q:'book:' + searchQuery,
+     }
+}
+   //callback function
+
+
+
+///////Render Functions//////////////////////////
+
+
+//////Event Handlers/////////////////////////////
